2abbae07986845623f0e32648c4fbe5fe6d7d60d
TGBenW <dukariga@gmail.com>
2021-11-27 14:48:23 +0200

    lesson02
    regular task

---
+++ script.js
@@ -0,0 +1,24 @@
+<<<<<<< Updated upstream
+=======
+let title = "TourAgency";
+let screens = "Simple, Complex, Interactive";
+let screenPrice = 1200;
+let rollback = 25;
+let fullPrice = 84350;
+let adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов " + screenPrice + " рублей");
+console.log("Стоимость разработки сайта " + fullPrice + " рублей");
+
+console.log(screens.toLowerCase());
+
+console.log(screens.split(", "));
+
+console.log(fullPrice * (rollback / 100));
+>>>>>>> Stashed changes
